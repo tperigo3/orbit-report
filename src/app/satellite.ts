6d73a13808b1aabac4779c54b683cb91bbeda72d
Garner Perigo <tperigo3@gmail.com>
2020-11-07 21:16:04 -0600

    finished part 7!

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,14 @@ export class Satellite {
       this.operational = operational;
       this.launchDate = launchDate;
     }    
+
+    shouldShowWarning(): boolean{
+        if (this.type.toLowerCase() === "space debris"){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 }
