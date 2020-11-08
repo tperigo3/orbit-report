500b8028436d9df76a4193de60bd28e005d7fe94
Garner Perigo <tperigo3@gmail.com>
2020-11-07 21:45:47 -0600

    finished part 8git status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    this.displayList = [];
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
@@ -20,8 +22,23 @@ export class AppComponent {
           let newSatellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(newSatellite);  
         }
-        // this.displayList = this.sourceList.slice(0);
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
+  }
+  
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+        let name = this.sourceList[i].name.toLowerCase();
+        if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+        }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+    
   }
 }
